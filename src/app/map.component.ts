3507a6a176a3090da5c3834ea7a1662d3236073a
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-07-06 21:12:18 +0300

    Merge branch 'master' of https://github.com/vladmikhbond/Guider
    # Conflicts:
    #	src/app/data/guider.service.ts
    #	src/app/guider.component.ts
    #	src/app/map.component.ts

--- src/app/map.component.ts
+++ src/app/map.component.ts
@@ -1,7 +1,8 @@
-﻿import {Component} from '@angular/core';
+﻿import {Component, OnInit} from '@angular/core';
 import {Vertex} from './data/vertex';
 
 const DASH_HEIGHT = 50;
+const SCALE_FACTOR = 1.2;
 
 @Component({
     selector: 'map',
@@ -13,6 +14,7 @@ const DASH_HEIGHT = 50;
             width: 100%;
             min-width: 320px;
             overflow: auto;
+            border: thin solid lightblue;
         }
     `],
     template: `
@@ -21,7 +23,7 @@ const DASH_HEIGHT = 50;
         </div>
         `
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
 
     bgImages: HTMLImageElement[];
     scrollBox: HTMLElement;
@@ -35,6 +37,10 @@ export class MapComponent {
     private floorNo: number = 0;
     private stepIdx: number;
 
+    ngOnInit(): void {
+        this.init();
+    }
+
     init() {
         // fill array of images
         this.bgImages = [];
@@ -45,9 +51,47 @@ export class MapComponent {
 
         // scrollBox size
         this.scrollBox = document.getElementById("scrollBox");
-        this.scrollBox.style.height = `${screen.height - DASH_HEIGHT}px`;
+        this.scrollBox.style.height = `${screen.height - DASH_HEIGHT - 2}px`;
         this.redraw();
+
+        let canvas = <HTMLCanvasElement>document.getElementById("canvas");
+        canvas.addEventListener("touchstart",  e => this.handleStart(e), false);
+        canvas.addEventListener("touchmove", e => this.handleMove(e), false);
     }
+
+    xt: number;
+    yt: number;
+
+    handleStart(evt: TouchEvent) {
+        evt.preventDefault();
+        if (evt.touches.length == 1) {
+            this.xt = evt.touches[0].clientX;
+            this.yt = evt.touches[0].clientY;
+        }  else if (evt.touches.length > 1) {
+            this.xt = Math.abs(evt.touches[0].clientX - evt.touches[1].clientX);
+            this.yt = Math.abs(evt.touches[0].clientY - evt.touches[1].clientY);
+        }
+    }
+
+    handleMove(evt: TouchEvent) {
+        evt.preventDefault();
+        let xt: number;
+        let yt: number;
+        if (evt.touches.length == 1) {
+            xt = evt.touches[0].clientX;
+            yt = evt.touches[0].clientY;
+            this.scrollBox.scrollLeft += this.xt - xt;
+            this.scrollBox.scrollTop += this.yt - yt;
+        } else if (evt.touches.length > 1) {
+            xt = Math.abs(evt.touches[0].clientX - evt.touches[1].clientX);
+            yt = Math.abs(evt.touches[0].clientY - evt.touches[1].clientY);
+            let k = this.xt < xt || this.yt < yt ? SCALE_FACTOR : 1 / SCALE_FACTOR;
+            this.scale *= k;
+        }
+        this.xt = xt;
+        this.yt = yt;
+    }
+
 
     private redraw() {
         let img = this.currentFloor;
@@ -132,4 +176,6 @@ export class MapComponent {
         this.redraw();
 
     }
+
+
 }
